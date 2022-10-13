c6a84ea67dc6bc2d1b62c4656bfb1ddeb3a2583f
Tarik Amar <tarik.amar@gmail.com>
2022-10-13 15:31:06 +0000

    Dirty! - Just the first basic call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      return await fetch(
+        'https://api.tarikamar.fr/wp-json/wp/v2/pages/5'
+        , {
+          credentials: 'include'
+          , mode: 'cors'
+        }
+      ).then(res => res.json())
+      .then(page => setPage(page))
+    }
+    const result = fetchData();
+    console.log(result);
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <p>WIP - Landing page soon</p>
+        <pre>{JSON.stringify(page)}</pre>
       </header>
     </div>
   );
