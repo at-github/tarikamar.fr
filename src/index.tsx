ac76dc43e74ffc3f58d75faa357964c35de99907
Tarik Amar <tarik.amar@gmail.com>
2022-11-02 09:29:16 +0000

    Route - Install & start to use router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
+
+function ErrorPage() {
+  const error = useRouteError()
+  // eslint-disable-next-line no-console
+  console.error(error)
+
+  return (
+    <>Ooops</>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/'
+    , element: <App />
+    , errorElement: <ErrorPage />
+  }, {
+    path: '/services'
+    , element: <App />
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// eslint-disable-next-line no-console
 reportWebVitals(console.log);
