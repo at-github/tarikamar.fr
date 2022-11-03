204cdd0c5d98e8e39a82dedb82ed1258cd188293
Tarik Amar <tarik.amar@gmail.com>
2022-11-03 15:47:27 +0000

    Scroll top - On click button right item menu is active

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,37 @@ import {HashLink} from 'react-router-hash-link';
 
 import './Footer.css';
 
+function ArrowUp() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      width="50"
+      height="50"
+      viewBox="-3 -2 30 30"
+      strokeWidth={2.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+      />
+    </svg>
+  )
+}
+
 export default function Footer() {
   return (
     <div id="footer">
-      <HashLink to={'#menu'}>🠑</HashLink>
+      <HashLink
+        to={'/#menu'}
+        id="scroll-top"
+        className="button"
+      >
+        <ArrowUp />
+      </HashLink>
     </div>
   )
 }
