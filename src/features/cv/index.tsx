c1875e3a5465fa1de0303ccda2b23e8de0def5fa
Tarik Amar <tarik.amar@gmail.com>
2022-11-30 00:15:08 +0000

    CV - Display education

---
+++ src/features/cv/index.tsx
@@ -0,0 +1,64 @@
+import useGetContent from '../../hooks/useGetContent'
+import {ExperienceInterface, Experience} from './Experience'
+import {FormationInterface, Formation} from './Formation'
+
+import CVContainer from './CVContainer'
+
+import './CV.css'
+
+function ExperienceWrapper(props: {
+  fetched: ExperienceInterface[]
+}) {
+  const experiences = props.fetched
+
+  return (
+    <>
+      {experiences.map((xp: ExperienceInterface) => {
+        return <Experience
+          title={xp.title.rendered}
+          content={xp.content.rendered}
+          company={xp.custom_fields.company}
+          location={xp.custom_fields.location}
+          period={xp.custom_fields.period}
+        />
+      })}
+    </>
+  )
+}
+
+function FormationWrapper(props: {
+  fetched: FormationInterface[]
+}) {
+  const formations = props.fetched
+
+  return (
+    <>
+      {formations.map((formation: any) => {
+        return <Formation
+          title={formation.title.rendered}
+          content={formation.content.rendered}
+          school={formation.custom_fields.school}
+          location={formation.custom_fields.location}
+          year={formation.custom_fields.year}
+        />
+      })}
+    </>
+  )
+}
+
+function ExperiencesController() {
+  return useGetContent(ExperienceWrapper, '/xp/experience')
+}
+
+function FormationsController() {
+  return useGetContent(FormationWrapper, '/edu/formation')
+}
+
+export default function CVController() {
+  return <CVContainer>
+    <>
+      <ExperiencesController />
+      <FormationsController />
+    </>
+  </CVContainer>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import Layout from './components/Layout';
 import Services from './features/services';
 import PostsController from './features/blog/Posts';
 import PostController from './features/blog/Post';
-import ExperiencesController from './features/cv/Experiences';
+import CVController from './features/cv';
 import NotFound from './features/notfound';
 import reportWebVitals from './reportWebVitals';
 
@@ -26,7 +26,7 @@ function App() {
         <Route path="services" element={<Services />} />
         <Route path="blog" element={<PostsController />} />
         <Route path="blog/:slug" element={<PostController />} />
-        <Route path="cv" element={<ExperiencesController />} />
+        <Route path="cv" element={<CVController />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
