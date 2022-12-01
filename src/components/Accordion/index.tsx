d3fe3cacd60cace9809f0677ea5b5aa28cc7a031
Tarik Amar <tarik.amar@gmail.com>
2022-12-01 00:00:33 +0000

    Accordion - Improve it

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,17 +1,48 @@
+import ChevronDoubleDown from '../Icons/ChevronDoubleDown'
+import ChevronDoubleUp from '../Icons/ChevronDoubleUp'
 import './Accordion.css'
 
 export default function Accordion(props: {
-  handleOpen: React.MouseEventHandler<HTMLDivElement>
+  handleOpen: React.MouseEventHandler<HTMLButtonElement>
   , open: Boolean
   , children: JSX.Element[]
+  , titleToShow: string
+  , titleToHide: string
 }) {
 
   return (
     <div
       className={`Accordion ${props.open ? 'show' : 'hide'}`}
-      onClick={props.handleOpen}
     >
+      {props.open ?
+        <button
+          className="read-more"
+          title={props.titleToHide}
+          onClick={props.handleOpen}
+        >
+          <ChevronDoubleUp />
+        </button> :
+        <button
+          className="read-more"
+          title={props.titleToShow}
+          onClick={props.handleOpen}
+        >
+          <ChevronDoubleDown />
+        </button>
+      }
+
       {props.children}
+
+      {props.open ?
+        <button
+          className="read-more"
+          title={props.titleToHide}
+          onClick={props.handleOpen}
+        >
+          <ChevronDoubleUp />
+        </button> :
+        null
+      }
     </div>
   )
 }
