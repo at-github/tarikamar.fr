63a7e77f3147cce90917710aa49d7919574de798
Tarik Amar <tarik.amar@gmail.com>
2023-01-05 08:40:48 +0000

    Service api - Tested

---
+++ src/tests/unit/services/api.test.tsx
@@ -0,0 +1,31 @@
+import myFetch from '../../../services/myFetch'
+import {postContact, get} from '../../../services/api'
+
+jest.mock('../../../services/myFetch');  // create an auto-mock of the module
+
+test('postContact call', () => {
+  const callback = jest.fn()
+
+  const body = new FormData()
+  body.append('key', 'value')
+  postContact(body, callback)
+
+  expect(myFetch).toHaveBeenCalledTimes(1)
+  expect(myFetch).toHaveBeenCalledWith(
+    '/contact-form-7/v1/contact-forms/49/feedback',
+    {
+      body
+      , method: 'POST'
+      , callback
+    }
+  )
+})
+
+test('get call', () => {
+  const body = new FormData()
+  body.append('key', 'value')
+  get('route')
+
+  expect(myFetch).toHaveBeenCalledTimes(1)
+  expect(myFetch).toHaveBeenCalledWith('route')
+})
