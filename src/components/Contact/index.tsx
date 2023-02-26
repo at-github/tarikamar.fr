a3e592123fc1a75b8a94187bc96287c0a309029b
Tarik Amar <tarik.amar@gmail.com>
2023-02-26 14:44:53 +0000

    Contact - Extract ContactForm component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,10 +1,8 @@
 import './Contact.css'
-import AlarmIcon from '../Icons/AlarmIcon'
-import CTA from '../CTA'
-import React, { useState } from 'react'
+import ContactForm from './ContactForm'
+import React, {useState} from 'react'
 import Thanking from './Thanking'
-import {Form, useActionData} from 'react-router-dom'
-import {buildInputStatusClass} from './contactHelper'
+import {useActionData} from 'react-router-dom'
 
 export default function Contact(props: {messagePlaceholder?: string}) {
   const {messagePlaceholder} = props
@@ -92,71 +90,4 @@ export default function Contact(props: {messagePlaceholder?: string}) {
       }
     }
   />
-}
-
-function ContactForm(props: {
-  email: {edited: boolean, valid: boolean}
-  , message: {edited: boolean, valid: boolean}
-  , errorsFromApi: {email: boolean, message: boolean}
-  , handleChangeEmail: React.ChangeEventHandler<HTMLInputElement>
-  , handleBlurEmail: React.ChangeEventHandler<HTMLInputElement>
-  , handleChangeMessage: React.ChangeEventHandler<HTMLTextAreaElement>
-  , handleBlurMessage: React.ChangeEventHandler<HTMLTextAreaElement>
-  , messagePlaceholder?: string
-}) {
-  const {
-    email
-    , message
-    , errorsFromApi
-    , handleChangeEmail
-    , handleBlurEmail
-    , handleChangeMessage
-    , handleBlurMessage
-    , messagePlaceholder
-  } = props
-
-  return (
-    <Form method="post" id="contact">
-      <div className={
-        buildInputStatusClass('form-row', email, errorsFromApi.email)
-      }>
-        <label className="error">
-          <AlarmIcon />
-          Désolé mais un email valide est requis
-        </label>
-
-        <input
-          type="email"
-          name="your-email"
-          placeholder="Votre email"
-          onChange={handleChangeEmail}
-          onBlur={handleBlurEmail}
-        />
-      </div>
-
-      <div className={
-        buildInputStatusClass('form-row', message, errorsFromApi.message)
-      }>
-        <label className="error">
-          <AlarmIcon />
-          Quelques mots pour décrire notre prise de contact peut-être ?
-        </label>
-        <textarea
-          name="your-message"
-          placeholder={messagePlaceholder ?? '…'}
-          rows={4}
-          cols={45}
-          onChange={handleChangeMessage}
-          onBlur={handleBlurMessage}
-        />
-      </div>
-
-      <div className="form-row">
-        <CTA
-          disabled={!(email.valid && message.valid)}
-          text="Contactez-moi"
-        />
-      </div>
-    </Form>
-  )
 }
